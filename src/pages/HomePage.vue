06b16f379762231cba9560db00ca148ef2b20f21
hakim <abdulhakim0401@gmail.com>
2024-11-09 13:38:02 +0800

    Delete check availability

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -16,12 +16,6 @@ import RoomCard from "../components/RoomCard.vue";
       </div>
 
       <!-- Search Section -->
-      <div class="flex space-x-4 mb-8  xl:justify-center border-solid ">
-        <input type="date" class="p-2 border rounded " placeholder="Check-in Date"/>
-        <input type="date" class="p-2 border rounded" placeholder="Check-out Date"/>
-        <input type="number" min="1" class="p-2 border rounded" placeholder="Guests"/>
-        <button class="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600">Check Availability</button>
-      </div>
 
       <!-- Rooms Section -->
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
